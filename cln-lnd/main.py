3d7ef7ffc6dddb9ef6ca9f9433dd7831c87e6994
Simone Samoggia <simone.samoggia2@studio.unibo.it>
2025-02-20 19:47:05 +0100

    Updated file paths and structure for JSON and GraphML exports; removed Cytoscape scripts; added new CSV files for network snapshots.

--- cln-lnd/main.py
+++ cln-lnd/main.py
@@ -5,8 +5,8 @@ from node_conversion import convert_nodes
 anno = "2021"
 
 # Specifica dei nomi dei file
-input_file = '../../topology/lntopo/' + str(anno) + '.json'
-output_file = '../json/lnd_' + str(anno) + '.json'
+input_file = '../topology/lntopo/' + str(anno) + '.json'
+output_file = '../jsons/lnd_' + str(anno) + '.json'
 
 # Lettura dei dati di input
 data = read_input_json(input_file)
--- transformer/main.py
+++ transformer/main.py
@@ -4,13 +4,13 @@ from export import export_graph_to_graphml, export_graph_to_json
 from datetime import datetime
 
 # Nomi delle cartelle di input e output
-input_folder = "../json"  # Cartella di input
+input_folder = "../jsons"  # Cartella di input
 output_folder = "../graphml"  # Cartella di output
 
-date = "2021-01-01" 
+date = "2025" 
 
 # Nomi dei file
-input_file_name = "lnd_2021"
+input_file_name = "lnd_" + str(date)
 graphml_file_name = "snapshot" + "_" + str(date)
 
 # Costruisci i percorsi dei file
@@ -18,10 +18,10 @@ input_file = f"{input_folder}/{input_file_name}.json"
 graphml_file = f"{output_folder}/{graphml_file_name}.graphml"
 
 # Data dello snapshot (estratta dal nome del file)
-snapshot_date = datetime.strptime(str(date), "%Y-%m-%d")
+# snapshot_date = datetime.strptime(str(date), "%Y-%m-%d")
 
 # Filtra i dati
-filtered_nodes_data, filtered_channels = filter_graph(input_file, snapshot_date)
+filtered_nodes_data, filtered_channels = filter_graph(input_file) # , snapshot_date)
 
 # Esporta il grafo in formato GraphML
 export_graph_to_graphml(filtered_nodes_data, filtered_channels, graphml_file)
